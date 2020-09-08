63d1668b4298fc1bf1fa9fc45d26f9f147ed831d
TheChiragRathod <crathod116@rku.ac.in>
2020-09-08 17:39:12 +0530

    register.xml design completed

--- app/src/main/java/com/rku/tutorial7/login.java
+++ app/src/main/java/com/rku/tutorial7/login.java
@@ -2,7 +2,9 @@ package com.rku.tutorial7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity
 {
@@ -13,5 +15,15 @@ public class login extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         this.setTitle("Login");
+    }
+
+
+
+
+    public void openRegister(View view)
+    {
+        Intent intent =new Intent(login.this,register.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/rku/tutorial7/register.java
+++ app/src/main/java/com/rku/tutorial7/register.java
@@ -10,5 +10,6 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        this.setTitle("Registration");
     }
 }
