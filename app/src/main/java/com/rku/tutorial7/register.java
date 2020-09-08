9ee8a9562a69a95ca400507e8383db42626a56a7
TheChiragRathod <crathod116@rku.ac.in>
2020-09-08 17:14:58 +0530

    i have been created register.java and register.xml file.

---
+++ app/src/main/java/com/rku/tutorial7/register.java
@@ -0,0 +1,14 @@
+package com.rku.tutorial7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
